d9ed46f711cb744336155443ce03f6f63cc8258f
Hyun Kyu Park <haymaker20@gmail.com>
2026-05-17 13:20:55 +0900

    spa로 변경

--- refer_rec_proc.php
+++
@@ -1,9 +0,0 @@
-<?
-include_once './admin/include/config.php';
-?>
-<?
-	if($HTTP_REFERER){
-		$query = "INSERT INTO refer_url (ip,referer,regdate) VALUES ('$_SERVER[REMOTE_ADDR]','$HTTP_REFERER',now() );";
-		$result = mysql_query($query, $connect);
-	}
-?>
